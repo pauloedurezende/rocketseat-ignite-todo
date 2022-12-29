332235b9554820f563492d60d0e5d55107e5c7df
Paulo Eduardo Rezende <rezendepauloeduardo@outlook.com>
2022-12-29 18:08:06 -0300

    feat: use light color on status bar

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { Home } from './src/screens';
 export default function App() {
   return (
     <>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
       <Home />
     </>
   );
