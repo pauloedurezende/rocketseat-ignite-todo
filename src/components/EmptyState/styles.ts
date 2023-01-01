659a94b93339ed088afdaa3d703d8d59f6a5a8ca
Paulo Eduardo Rezende <pauloeduardorezende@outlook.com>
2023-01-01 02:48:14 -0300

    feat: add EmptyState component

---
+++ src/components/EmptyState/styles.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 48,
+    paddingHorizontal: 20,
+    alignItems: 'center',
+  },
+  row: {
+    marginTop: 16,
+    alignItems: 'center',
+  },
+  title: {
+    fontWeight: 'bold',
+    color: '#808080',
+    fontSize: 14,
+  },
+  subtitle: {
+    color: '#808080',
+    fontSize: 14,
+  },
+});
