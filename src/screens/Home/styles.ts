7f89ff6f76283b9e50c7c247be99fc4a817c095d
Paulo Eduardo Rezende <pauloeduardorezende@outlook.com>
2023-01-01 02:30:46 -0300

    feat: add initial header styles

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#1a1a1a',
+    flex: 1,
+  },
+  header: {
+    height: 173,
+    backgroundColor: '#0d0d0d',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
