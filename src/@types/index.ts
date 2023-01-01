50f1a4266e7a9790c41b2195e1556b52f23f6023
Paulo Eduardo Rezende <rezendepauloeduardo@outlook.com>
2023-01-01 03:31:40 -0300

    feat: add functionality to add and display tasks

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -2,3 +2,9 @@ export enum ButtonState {
   Active = 'active',
   Normal = 'normal',
 }
+
+export type TaskItem = {
+  id: string;
+  title: string;
+  done: boolean;
+};
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   View,
   Image,
@@ -5,15 +6,30 @@ import {
   TouchableOpacity,
   FlatList,
 } from 'react-native';
+import { nanoid } from 'nanoid';
 
 import { styles } from './styles';
 
-import { EmptyState, TaskInfo } from '../../components';
+import { EmptyState, Task, TaskInfo } from '../../components';
 
 import logo from '../../../assets/logo.png';
 import plus from '../../../assets/plus.png';
 
+import { TaskItem } from '../../@types';
+
 export default function Home() {
+  const [input, setInput] = useState('');
+  const [tasks, setTasks] = useState<TaskItem[]>([]);
+
+  function handleTaskCreate() {
+    if (input.length === 0) return;
+
+    const task = { id: nanoid(), title: input, done: false };
+
+    setTasks((prevState) => [...prevState, task]);
+    setInput('');
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -23,12 +39,14 @@ export default function Home() {
       <View style={styles.main}>
         <View style={styles.form}>
           <TextInput
+            style={styles.input}
             placeholder="Add a new task"
             placeholderTextColor="#808080"
-            style={styles.input}
+            onChangeText={setInput}
+            value={input}
           />
 
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity style={styles.button} onPress={handleTaskCreate}>
             <Image source={plus} />
           </TouchableOpacity>
         </View>
@@ -45,9 +63,14 @@ export default function Home() {
           </View>
 
           <FlatList
-            data={[]}
+            data={tasks}
+            keyExtractor={(item) => item.id}
             showsVerticalScrollIndicator={false}
-            renderItem={() => null}
+            renderItem={({ item }) => (
+              <Task key={item.id} done={item.done}>
+                {item.title}
+              </Task>
+            )}
             ListEmptyComponent={() => <EmptyState />}
           />
         </View>
