f34077a02f96d587ce823d3b986af4be54908871
Paulo Eduardo Rezende <rezendepauloeduardo@outlook.com>
2023-01-01 02:35:09 -0300

    feat: add initial form structure to handle new tasks

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -11,4 +11,32 @@ export const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  main: {
+    top: -31,
+    paddingHorizontal: 24,
+  },
+  form: {
+    flexDirection: 'row',
+    paddingBottom: 32,
+  },
+  input: {
+    height: 54,
+    padding: 16,
+    flex: 1,
+    marginRight: 4,
+    color: '#f2f2f2',
+    backgroundColor: '#262626',
+    borderColor: '#0d0d0d',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderRadius: 6,
+  },
+  button: {
+    width: 52,
+    height: 52,
+    backgroundColor: '#1e6f9f',
+    borderRadius: 6,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
