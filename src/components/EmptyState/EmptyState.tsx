0d058bc43ea2762a4a404d6b2c95d1e4448baeee
Paulo Eduardo Rezende <rezendepauloeduardo@outlook.com>
2023-01-01 02:48:14 -0300

    feat: add EmptyState component

---
+++ src/components/EmptyState/EmptyState.tsx
@@ -0,0 +1,20 @@
+import { View, Text, Image } from 'react-native';
+
+import { styles } from './styles';
+
+import clipboard from '../../../assets/clipboard.png';
+
+export default function EmptyState() {
+  return (
+    <View style={styles.container}>
+      <Image source={clipboard} />
+
+      <View style={styles.row}>
+        <Text style={styles.title}>You don't have tasks registered yet</Text>
+        <Text style={styles.subtitle}>
+          Create tasks and organize your to-do items
+        </Text>
+      </View>
+    </View>
+  );
+}
