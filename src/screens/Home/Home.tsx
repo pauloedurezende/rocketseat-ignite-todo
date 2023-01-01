2e7178d520a0fdd9a2c8f8b239536caa6b245240
Paulo Eduardo Rezende <pauloeduardorezende@outlook.com>
2023-01-01 02:35:09 -0300

    feat: add initial form structure to handle new tasks

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,14 +1,29 @@
-import { View, Image } from 'react-native';
+import { View, Image, TextInput, TouchableOpacity } from 'react-native';
 
 import { styles } from './styles';
 
 import logo from '../../../assets/logo.png';
+import plus from '../../../assets/plus.png';
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Image source={logo} />
+      </View>
+
+      <View style={styles.main}>
+        <View style={styles.form}>
+          <TextInput
+            placeholder="Add a new task"
+            placeholderTextColor="#808080"
+            style={styles.input}
+          />
+
+          <TouchableOpacity style={styles.button}>
+            <Image source={plus} />
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
